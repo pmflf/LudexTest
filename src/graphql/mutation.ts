e523f38fcbfa9c732418667bffadca3b3a44b57d
Yun Chen Qian <yun.qian@dawsoncollege.qc.ca>
2025-02-21 13:17:55 -0500

    style: fix style errors

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -1,5 +1,5 @@
-import { type MutationResolvers as IMutation } from "./generated/graphql";
-import { Context } from "./context";
+import type { MutationResolvers as IMutation } from "./generated/graphql";
+import type { Context } from "./context";
 
 export const Mutation: IMutation<Context> = {
   createSomething: async (_, { input }, { prisma }) => {
--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,5 +1,5 @@
-import { type QueryResolvers as IQuery } from "./generated/graphql";
-import { Context } from "./context";
+import type { QueryResolvers as IQuery } from "./generated/graphql";
+import type { Context } from "./context";
 
 export const Query: IQuery<Context> = {
   hello: () => "world",

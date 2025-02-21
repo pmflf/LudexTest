ae2315bc93f08ff984e1ffa3afc7602361dd40b2
Yun Chen Qian <yun.qian@dawsoncollege.qc.ca>
2025-02-21 16:57:15 -0500

    feat: define todo and date scalar

--- src/graphql/typeDefinitions.ts
+++ src/graphql/typeDefinitions.ts
@@ -1,4 +1,6 @@
 export const typeDefs = /* GraphQL */ `
+  scalar Date @specifiedBy(url: "https://scalars.graphql.org/andimarek/date-time")
+
   input CreateSomethingInput {
     name: String!
   }
@@ -8,8 +10,20 @@ export const typeDefs = /* GraphQL */ `
     name: String!
   }
 
+  input CreateTodoInput {
+    title: String!
+    dueDate: Date!
+  }
+
+  type Todo {
+    id: ID!
+    title: String!
+    dueDate: Date!
+  }
+
   type Mutation {
     createSomething(input: CreateSomethingInput!): Something!
+    createTodo(input: CreateTodoInput!): Todo!
   }
 
   type Query {

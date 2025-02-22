890cd337bf12cb3e1e26b605655003ec1ce87334
Yun Chen Qian <yun.qian@dawsoncollege.qc.ca>
2025-02-22 17:14:38 -0500

    feat: implemented update todo complete status

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -2,31 +2,51 @@ import type { MutationResolvers as IMutation } from "./generated/graphql";
 import type { Context } from "./context";
 
 export const Mutation: IMutation<Context> = {
-  createSomething: async (_, { input }, { prisma }) => {
-    const something = await prisma.something.create({
-      data: {
-        name: input.name,
-      },
-    });
+	createSomething: async (_, { input }, { prisma }) => {
+		const something = await prisma.something.create({
+			data: {
+				name: input.name,
+			},
+		});
 
-    return {
-      id: something.id,
-      name: something.name,
-    };
-  },
-  //NOTE - creates a todo with a specific format of date https://scalars.graphql.org/andimarek/date-time.html
-  createTodo: async (_, { input }, { prisma }) => {
-    const todo = await prisma.todo.create({
-      data: {
-        title: input.title,
-        dueDate: input.dueDate,
-      },
-    });
+		return {
+			id: something.id,
+			name: something.name,
+		};
+	},
+	//NOTE - creates a todo with a specific format of date https://scalars.graphql.org/andimarek/date-time.html
+	createTodo: async (_, { input }, { prisma }) => {
+		const todo = await prisma.todo.create({
+			data: {
+				title: input.title,
+				dueDate: input.dueDate,
+			},
+		});
 
-    return {
-      id: todo.id,
-      title: todo.title,
-      dueDate: input.dueDate
-    };
-  },
+		return {
+			id: todo.id,
+			title: todo.title,
+			completed: todo.completed,
+			dueDate: todo.dueDate,
+		};
+	},
+
+	//NOTE - updates the completed status of a todo
+	updateTodoCompletion: async (_, { input }, { prisma }) => {
+		const todo = await prisma.todo.update({
+			where: {
+				id: input.id,
+			},
+			data: {
+				completed: input.completed,
+			},
+		});
+
+		return {
+			id: todo.id,
+			title: todo.title,
+			completed: todo.completed,
+      dueDate: todo.dueDate
+		};
+	},
 };
